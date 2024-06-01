5680d960832a15b946d7aaf151aad86a0dff263c
Paolo <peraltadavidpaolo@gmail.com>
2024-06-02 00:13:09 +0800

    update state bucket

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "cos" {
     region = "ap-singapore"
-    bucket = "dynamic_env-dynamic_proj-state-1324350682"
+    bucket = "dynamic_env-sndz-state-1324350682"
     key   = "dynamic_env/dynamic_env-dynamic_proj-state.tfstate"
   }
 }
